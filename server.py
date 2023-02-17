c01d1cadb3525bee9ba4f92b53c6effa6fe5c290
guilevieiram <guilhermevmanhaes@gmail.com>
2023-02-17 20:57:21 +0100

    docs on python server

--- server.py
+++ server.py
@@ -6,16 +6,17 @@ app = Flask(__name__)
 COUNTER = 0
 @app.route("/")
 def home():
+    """Home method used to define the counter endpoint."""
     global COUNTER
     COUNTER += 1
     return f"Hello counter {COUNTER}\n"
 
 @app.route("/getUrl", methods=["GET"])
 def get_url():
+    """Endpoint to perform a get request to the passed url."""
     url = request.args.get("url")
-    res = requests.get(url)
+    res = requests.get(url, timeout=10)
     return res.text + "\n"
 
 if __name__=="__main__":
     app.run(host="10.0.0.2", port="8000")
-
